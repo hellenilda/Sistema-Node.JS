ef91bc9793b3e906ce069d39689bff39a93d5aec
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-02 17:00:07 -0300

    feat: conexao.js

--- conexao.js
+++ conexao.js
@@ -3,3 +3,32 @@ const sequelize = new Sequelize('banco','root','senhaAqui', {
     host: 'localhost',
     dialect: 'mysql'
 })
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+/* Sincroniza a tabela com o BD
+(só precisa executar uma vez) */
+Postagem.sync({force: true})
+Usuario.sync({force: true})
