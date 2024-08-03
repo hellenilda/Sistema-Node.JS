29bb67417e732c797e5be48987990e059fb69449
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-03 00:36:54 -0300

    feat: /view

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express()
 PORT = 3000
 
 app.get('/', function(req, res) {
-    res.send('Funcionando')
+    res.sendFile(__dirname + '/view/home.html')
 })
 
-app.get('/login/:nome', function(req, res) {
-    res.send('Olá, '+req.params.nome)
+app.get('/login', function(req, res) {
+    res.send('Bem vindo!')
 })
 
 app.get('/Cadastro', function(req, res) {
