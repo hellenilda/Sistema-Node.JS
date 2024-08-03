beaf6f7e5736f5b4fd2b0e366b35b7883769a908
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-03 00:33:10 -0300

    feat: teste de /rota/:parametro

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const app = require('app.js')
-const { Sequelize } = require('sequelize')
+const express = require('express')
+const app = express()
+PORT = 3000
 
-app.get('/', (req, res) => {
-    console.log('Funcionando')
+app.get('/', function(req, res) {
+    res.send('Funcionando')
+})
+
+app.get('/login/:nome', function(req, res) {
+    res.send('Olá, '+req.params.nome)
+})
+
+app.get('/Cadastro', function(req, res) {
+    res.send('Cadastro')
+})
+
+app.listen(PORT, function(){
+    console.log('Servidor rodando na porta ' + PORT)
 })
