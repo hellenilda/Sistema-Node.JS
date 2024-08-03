76160f4842e26be538923715d8d4f1891c8ea42e
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-03 00:53:39 -0300

    feat: /views/layouts

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 PORT = 3000
 
-
+// Configurações
+    // Usando o Handlebars como Template Engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine','handlebars')
 
 app.listen(PORT, function(){
     console.log('Servidor rodando na porta ' + PORT)
