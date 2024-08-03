aaaf93dbb2f3fd1eee989dfe4690b0874a011e08
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-03 00:45:07 -0300

    env: handlebars

--- app.js
+++ app.js
@@ -2,17 +2,7 @@ const express = require('express')
 const app = express()
 PORT = 3000
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/view/home.html')
-})
 
-app.get('/login', function(req, res) {
-    res.send('Bem vindo!')
-})
-
-app.get('/Cadastro', function(req, res) {
-    res.send('Cadastro')
-})
 
 app.listen(PORT, function(){
     console.log('Servidor rodando na porta ' + PORT)
